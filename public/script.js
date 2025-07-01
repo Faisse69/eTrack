233c6024af2676d7165b101afe9f5a8b1a39ca46
Faisse69 <135174415+Faisse69@users.noreply.github.com>
2025-07-01 16:34:17 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1087,4 +1087,4 @@ function formatNumberWithSpaces(number) {
 
 
 
-create_message_box('info', 'Everything on Eclipse', '<a href="https://everything-on-eclipse.xyz">Click here to explore all the Eclipse ecosysteme. <br></a> By <a href="https://x.com/Yvr78f">eOrbite</a>');
+create_message_box('info', 'Everything on Eclipse', '<a href="https://everything-on-eclipse.xyz" style="border:none">Click here to explore all the Eclipse ecosysteme. <br> By eOrbite</a>');
